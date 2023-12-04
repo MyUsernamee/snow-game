5afb9ff906acd9042c067aa1935344b30911e223
MyUsername <delanoleslieprof@gmail.com>
2023-12-04 14:14:57 -0800

    Update player movement speed

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -6,10 +6,13 @@ class_name Player
 @onready var camera = $CharacterBody3D/Camera3D
 
 @export var mouse_sensitivity: float = 0.1
-@export var move_speed: float = 10
-@export var sprint_speed: float = 20
+@export var move_speed: float = 4
+@export var sprint_speed: float = 10
 @export var jump_power: float = 10
 @export var gravity: float = 9.8 * 4
+
+var sprint_meter: float = 0 # TODO: Implement sprint meter
+var cold_meter: float = 0 # TODO: Implement cold meter
 
 var view_direction: Vector3 = Vector3(0, 0, 1)
 
@@ -26,7 +29,12 @@ func _physics_process(delta):
 	movement.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	movement.z = Input.get_action_strength("move_forward") - Input.get_action_strength("move_back")
 
-	movement = movement.normalized() * move_speed
+	var local_move_speed = move_speed
+
+	if Input.is_action_pressed("sprint"):
+		local_move_speed = sprint_speed
+
+	movement = movement.normalized() * local_move_speed
 
 	if Input.is_action_just_pressed("jump") and character_body.is_on_floor():
 		movement.y = jump_power
